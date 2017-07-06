4d0dd7b14d9c5b9a2d890e229240fedb495b82a7
Freezerburn26 <Freezerburn26@gmail.com>
2017-07-06 10:56:26 -0400

    FizzBuzz added

---
+++ FizzBuzz/FizzBuzz.cc
@@ -0,0 +1,28 @@
+/*
+Straight forward fizzbuzz solution in C++
+by Donald Purvis
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for( int i = 1; i <= 100; i++)
+    {
+        if ( (i % 3 == 0) && (i % 5 == 0))
+        {
+            cout << "fizzbuzz" << endl;
+        }else if (i % 3 == 0)
+        {
+            cout << "fizz" << endl;
+        }else if (i % 5 == 0)
+        {
+            cout << "buzz" << endl;
+        }else
+        {
+            cout << i << endl;
+        }
+    }
+}
